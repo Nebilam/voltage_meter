c756e88b0caef04b559c19222541f505e92994ed
Nebilam <49345234+Nebilam@users.noreply.github.com>
2020-06-15 17:21:30 +0200

    Commit 2

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ let millivolts = 0
 basic.forever(function () {
     millivolts = Math.ceil(pins.analogReadPin(AnalogPin.P0) * 3000 / 1023)
     basic.showNumber(millivolts)
-    serial.writeNumber(millivolts)
+    serial.writeValue("Millivolts", millivolts)
 })
