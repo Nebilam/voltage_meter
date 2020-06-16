037e90836245a0c57e84d386107c181a0b8bec21
Nebilam <49345234+Nebilam@users.noreply.github.com>
2020-06-16 13:45:19 +0200

    Test met verschillende formules.

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
+let knop = 0
 let millivolts = 0
+input.onButtonPressed(Button.A, function () {
+    knop = 0
+})
+input.onButtonPressed(Button.B, function () {
+    knop = 1
+})
 basic.forever(function () {
-    millivolts = Math.round(pins.analogReadPin(AnalogPin.P0) * 3000 / 1023)
+    if (knop == 0) {
+        millivolts = Math.round(pins.analogReadPin(AnalogPin.P0) * 3000 / 1023)
+    } else {
+        millivolts = Math.round(pins.analogReadPin(AnalogPin.P0) * 1000 / 340)
+    }
     basic.showNumber(millivolts)
     serial.writeValue("Millivolts", millivolts)
 })
